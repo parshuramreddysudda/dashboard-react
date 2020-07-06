0694b30ab5e829604e570c03219321cf7bf3e2b4
Parshuarm Reddy <parshuram.sudda@gmail.com>
2020-07-06 20:39:40 +0530

    Update machine.action.spec.js

--- src/Machines/Services/machine.action.spec.js
+++ src/Machines/Services/machine.action.spec.js
@@ -107,26 +107,45 @@ describe('Testing Machine Component Actions ', () => {
         jest.spyOn(store, 'getState').mockImplementation(() => { })
         const params = {}
         return store.dispatch(machineActions.fetchMachines(params)).then(() => {
-            const actions = store.getActions();
-            expect(actions[0].type).toEqual('FETCH_MACHINES_PENDING');
-            expect(actions[1].type).toEqual('FETCH_MACHINES_SUCCESS');
-            expect(actions[2].type).toEqual('FETCH_MACHINES_COMPLETED');
-            const machine = actions[1].payload;
-            machine.forEach(machine => {
-                expect(machine).toHaveProperty('id');
-                expect(machine).toHaveProperty('name');
-                expect(machine).toHaveProperty('os');
-                expect(machine).toHaveProperty('ip');
-                expect(machine).toHaveProperty('mac');
-            });
 
-        }).catch(() => {
-            const actions = store.getActions();
-            expect(actions[0].type).toEqual('FETCH_MACHINES_PENDING');
-            expect(actions[1].type).toEqual('FETCH_MACHINES_ERROR');
-            expect(actions[2].type).toEqual('FETCH_MACHINES_COMPLETED');
+            try {
+                const actions = store.getActions();
+                expect(actions[0].type).toEqual('FETCH_MACHINES_PENDING');
+                expect(actions[1].type).toEqual('FETCH_MACHINES_SUCCESS');
+
+                const machine = actions[1].payload;
+                machine.forEach(machine => {
+                    expect(machine).toHaveProperty('id');
+                    expect(machine).toHaveProperty('name');
+                    expect(machine).toHaveProperty('os');
+                    expect(machine).toHaveProperty('ip');
+                    expect(machine).toHaveProperty('mac');
+                });
+            } finally {
+                const actions = store.getActions();
+                expect(actions[2].type).toEqual('FETCH_MACHINES_COMPLETED');
+            }
         })
     })
+    test('should create FETCH_MACHINES_PENDING when fetching app is started and FETCH_MACHINES_ERROR when fetching is failed and FETCH_MACHINES_COMPLETED finally completed  ', () => {
+        const initialState = {}
+        const store = mockStore(initialState)
+        jest.spyOn(store, 'getState').mockImplementation(() => { })
+        const params = { id: '%%%%' }
+        return store.dispatch(machineActions.fetchMachines(params)).then(() => {
+            try {
+                const actions = store.getActions();
+            } catch (error) {
+                const actions = store.getActions();
+                expect(actions[0].type).toEqual('FETCH_MACHINES_PENDING');
+                expect(actions[1].type).toEqual('FETCH_MACHINES_ERROR');
+                expect(actions[2].type).toEqual('FETCH_MACHINES_COMPLETED');
+
+            }
+        }
+        )
+    })
+
     test('should create CREATE_MACHINE_PENDING when Creating app is started and CREATE_MACHINE_SUCCESS when Creating is done and CREATE_MACHINE_COMPLETED finally completed  ', () => {
         const initialState = {}
         const store = mockStore(initialState)
@@ -139,22 +158,46 @@ describe('Testing Machine Component Actions ', () => {
             "mac": "99:9I:32:Y9:6J:E7"
         }
         return store.dispatch(machineActions.createMachine(params)).then(() => {
-            const actions = store.getActions();
-            expect(actions[0].type).toEqual('CREATE_MACHINES_PENDING');
-            expect(actions[1].type).toEqual('CREATE_MACHINES_SUCCESS');
-            expect(actions[2].type).toEqual('CREATE_MACHINES_COMPLETED');
-            const machine = actions[1].payload.params;
-            expect(machine).toHaveProperty('id');
-            expect(machine).toHaveProperty('name');
-            expect(machine).toHaveProperty('os');
-            expect(machine).toHaveProperty('ip');
-            expect(machine).toHaveProperty('mac');
+            try {
+                const actions = store.getActions();
+                expect(actions[0].type).toEqual('CREATE_MACHINES_PENDING');
+                expect(actions[1].type).toEqual('CREATE_MACHINES_SUCCESS');
 
-        }).catch(() => {
-            const actions = store.getActions();
-            expect(actions[0].type).toEqual('CREATE_MACHINES_PENDING');
-            expect(actions[1].type).toEqual('CREATE_MACHINES_ERROR');
-            expect(actions[2].type).toEqual('CREATE_MACHINES_COMPLETED');
+                const machine = actions[1].payload.params;
+                expect(machine).toHaveProperty('id');
+                expect(machine).toHaveProperty('name');
+                expect(machine).toHaveProperty('os');
+                expect(machine).toHaveProperty('ip');
+                expect(machine).toHaveProperty('mac');
+
+            } finally {
+                const actions = store.getActions();
+                expect(actions[2].type).toEqual('CREATE_MACHINES_COMPLETED');
+            }
+
+        })
+    })
+    test('should create CREATE_MACHINE_PENDING when Creating app is started and CREATE_MACHINE_ERROR when Creating is failed and CREATE_MACHINE_COMPLETED finally completed  ', () => {
+        const initialState = {}
+        const store = mockStore(initialState)
+        jest.spyOn(store, 'getState').mockImplementation(() => { })
+        const params = {
+            "id": "67asdd2b441-e565-b1a7-1770-19300c29c9ac%%%%",
+            "name": "qusadis aute minim ad",
+            "os": "tyasdpe-not-found",
+            "ip": "23343.222.151.112",
+            "mac": "99:9I:32:Y9:6J:E7"
+        }
+        return store.dispatch(machineActions.createMachine(params)).then(() => {
+            try {
+                const actions = store.getActions();
+            } catch (error) {
+                const actions = store.getActions();
+                expect(actions[0].type).toEqual('CREATE_MACHINES_PENDING');
+                expect(actions[1].type).toEqual('CREATE_MACHINES_ERROR');
+                expect(actions[2].type).toEqual('CREATE_MACHINES_COMPLETED');
+
+            }
         })
     })
     test('should create DELETE_MACHINE_PENDING when Creating app is started and DELETE_MACHINE_SUCCESS when Creating is done and DELETE_MACHINE_COMPLETED finally completed  ', () => {
@@ -179,7 +222,7 @@ describe('Testing Machine Component Actions ', () => {
             }
         })
     })
-    test('should create DELETE_MACHINE_PENDING when Creating app is started and DELETE_MACHINE_SUCCESS when Creating is failed and DELETE_MACHINE_COMPLETED finally completed  ', () => {
+    test('should create DELETE_MACHINE_PENDING when Creating app is started and DELETE_MACHINE_ERROR when Creating is failed and DELETE_MACHINE_COMPLETED finally completed  ', () => {
         const initialState = {}
         const store = mockStore(initialState)
         const params = '67d2b441-e565-b1a7-1770-19300c2a%%%'
@@ -194,19 +237,6 @@ describe('Testing Machine Component Actions ', () => {
                 expect(actions[1].type).toEqual('DELETE_MACHINES_ERROR');
                 expect(actions[2].type).toEqual('DELETE_MACHINES_COMPLETED');
             }
-        })
-    })
-    test('should create DELETE_MACHINE_PENDING when Creating app is started and DELETE_MACHINE_SUCCESS when Creating is failed and DELETE_MACHINE_COMPLETED finally completed  ', () => {
-        const initialState = {}
-        const store = mockStore(initialState)
-        const params = '67d2b441-e565-b1a7-1770-19300c29/asd'
-        return store.dispatch(machineActions.deleteMachine(params)).then(() => {
-
-        }).catch((e) => {
-            const actions = store.getActions();
-            expect(actions[0].type).toEqual('DELETE_MACHINES_PENDING');
-            expect(actions[1].type).toEqual('DELETE_MACHINES_ERROR');
-            expect(actions[2].type).toEqual('DELETE_MACHINES_COMPLETED');
         })
     })
     test('should create UPDATE_MACHINE_PENDING when updating app is started and UPDATE_MACHINE_SUCCESS when update is done and UPDATE_MACHINE_COMPLETED finally completed  ', () => {
@@ -244,6 +274,64 @@ describe('Testing Machine Component Actions ', () => {
             }
         ]
         const id = '6be7b591-fc4f-3eca-7128-3a52c30366e2'
+        try {
+            return store.dispatch(machineActions.updateMachine(params, newData, id)).then(() => {
+                const actions = store.getActions();
+                // console.log(actions)
+                expect(actions[0].type).toEqual('UPDATE_MACHINES_PENDING');
+                expect(actions[1].type).toEqual('UPDATE_MACHINES_SUCCESS');
+                const machine = actions[1].payload.updateMachine;
+                // app.forEach(app => {
+                // console.log(app)
+                expect(machine).toHaveProperty('id');
+                expect(machine).toHaveProperty('name');
+                expect(machine).toHaveProperty('os');
+                expect(machine).toHaveProperty('ip');
+                expect(machine).toHaveProperty('mac');
+                // });
+            })
+        } finally {
+            const actions = store.getActions();
+            expect(actions[2].type).toEqual('UPDATE_MACHINES_COMPLETED');
+
+        }
+    })
+
+    test('should create UPDATE_MACHINE_PENDING when updating app is started and UPDATE_MACHINE_SUCCESS when update is done and UPDATE_MACHINE_COMPLETED finally completed  ', () => {
+        const initialState = {}
+        const store = mockStore(initialState)
+        const params = [
+            {
+                "id": "67d2b441-e565-b1a7-1770-1930%%%0c29c9ac",
+                "name": "quis aute minim ad",
+                "os": "type-not-found",
+                "ip": "233.222.151.112",
+                "mac": "99:9I:32:Y9:6J:E7"
+            },
+            {
+                "id": "42d5abb8-1416-6630-331c-c3d%%%%a79c25f28",
+                "name": "in Excepteur",
+                "os": "Unix",
+                "ip": "124.241.141.134",
+                "mac": "78:6R:18:J9:8E:C6"
+            }]
+        const newData = [
+            {
+                "id": "67d2b441-e665-b1a7-1770-19300c29c9ac",
+                "name": "quis aute minim ad",
+                "os": "type-found",
+                "ip": "233.2892.151.112",
+                "mac": "99:9I:32:Y9:6J:E7"
+            },
+            {
+                "id": "42d5abb8-1416-6630-331c-c3da79%%%c25f28",
+                "name": "in Excepteur",
+                "os": "Unix",
+                "ip": "154.241.141.134",
+                "mac": "78:6R:18:J9:8E:C6"
+            }
+        ]
+        const id = '6be7b591-fc4f-3eca-7128-3a52c30366e2'
         return store.dispatch(machineActions.updateMachine(params, newData, id)).then(() => {
             const actions = store.getActions();
             // console.log(actions)
@@ -266,8 +354,5 @@ describe('Testing Machine Component Actions ', () => {
             expect(actions[2].type).toEqual('UPDATE_MACHINES_COMPLETED');
         })
     })
-
-
-
 
 })
